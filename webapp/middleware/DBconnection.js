e6cfa8ad55e5a3129f4d966f2540d500b5405047
Stefano Taillefert <stefano.taille@gmail.com>
2021-11-17 10:55:59 +0100

    Add mongo connection

--- webapp/middleware/DBconnection.js
+++ webapp/middleware/DBconnection.js
@@ -1,5 +1,7 @@
 const neo4j = require("neo4j-driver");
+const MongoClient = require("mongodb").MongoClient;
 
+// neo4j connection
 let driver, session;
 try {
   driver = neo4j.driver(
@@ -13,13 +15,29 @@ try {
     }
   );
   session = driver.session();
-  console.info("Connection to database successful");
+  console.info("Connection to neo4j database successful");
 } catch (e) {
   console.error(
-    "Failed to connect to the database! Check that you set the correct env variables (see README for details)"
+    "Failed to connect to the neo4j database! Check that you set the correct env variables (see README for details)"
   );
 }
 
+//mongoDB connection
+const mongoURI = process.env.MONGODB_URI || "mongodb://mongo/ddm-project";
+let db;
+MongoClient.connect(mongoURI, function (err, client) {
+  if (err) {
+    console.error(
+      "Failed to connect to the mongoDB database! Check that you set the correct env variables (see README for details)"
+    );
+    console.error(err);
+    return;
+  }
+  console.info("Connection to mongoDB database successful");
+  db = client.db("ddm-project");
+});
+
+//query execution (neo4j)
 async function executeQuery(statement, params = {}) {
   try {
     return session.run(statement, params);
@@ -29,4 +47,15 @@ async function executeQuery(statement, params = {}) {
     throw error;
   }
 }
-module.exports = { executeQuery };
+
+//query execution (mongoDB)
+async function executeMongoQuery(query) {
+  try {
+    return db.collection("certificates").find(query);
+  } catch (error) {
+    // Throw up
+    console.error(error);
+    throw error;
+  }
+}
+module.exports = { executeQuery, executeMongoQuery };
