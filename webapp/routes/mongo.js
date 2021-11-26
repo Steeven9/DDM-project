5f3b3778dc46a5dac574d54e5108a4241ae72529
Stefano Taillefert <stefano.taille@gmail.com>
2021-11-26 17:12:53 +0100

    Add auth to mongo route

--- webapp/routes/mongo.js
+++ webapp/routes/mongo.js
@@ -4,7 +4,6 @@ const { ourMongo } = require("../middleware/DBconnection");
 
 // see https://docs.mongodb.com/drivers/node/current/usage-examples/
 
-// eslint-disable-next-line no-unused-vars
 const assertPassword = (req) =>
   new Promise((resolve, reject) => {
     const auth = req.headers.authorization;
@@ -20,14 +19,20 @@ const assertPassword = (req) =>
  * Returns an array of documents or 404 if none.
  */
 router.get("/all", async (req, res) => {
-  let docs = [];
-  let db = await ourMongo();
-  let result = await db.find({});
-  await result.forEach(docs.push);
-  if (docs.length > 0) {
-    res.send(result);
-  } else {
-    res.status(404).send("No documents in the database");
+  try {
+    await assertPassword(req);
+    let docs = [];
+    let db = await ourMongo();
+    let result = await db.find({});
+    await result.forEach(docs.push);
+    if (docs.length > 0) {
+      res.send(result);
+    } else {
+      res.status(404).send("No documents in the database");
+    }
+  } catch (error) {
+    console.error(error);
+    res.send(error);
   }
 });
 
@@ -37,14 +42,20 @@ router.get("/all", async (req, res) => {
  * Returns an array of documents or 404 if none.
  */
 router.get("/", async (req, res) => {
-  let docs = [];
-  let db = await ourMongo();
-  let result = await db.find(req.body.query);
-  await result.forEach(docs.push);
-  if (docs.length > 0) {
-    res.send(result);
-  } else {
-    res.status(404).send("No documents found");
+  try {
+    await assertPassword(req);
+    let docs = [];
+    let db = await ourMongo();
+    let result = await db.find(req.body.query);
+    await result.forEach(docs.push);
+    if (docs.length > 0) {
+      res.send(result);
+    } else {
+      res.status(404).send("No documents found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.send(error);
   }
 });
 
@@ -53,13 +64,21 @@ router.get("/", async (req, res) => {
  * Returns 201 on success or 500 on failure.
  */
 router.post("/insert", async (req, res) => {
-  let db = await ourMongo();
-  let result = await db.insertMany(req.body.documents);
-  if (Number(result.result.ok)) {
-    res.status(201).send(result.insertedCount + " docs inserted successfully");
-  } else {
-    console.error("Error inserting docs ", result);
-    res.status(500).send("Error inserting docs ", result);
+  try {
+    await assertPassword(req);
+    let db = await ourMongo();
+    let result = await db.insertMany(req.body.documents);
+    if (Number(result.result.ok)) {
+      res
+        .status(201)
+        .send(result.insertedCount + " docs inserted successfully");
+    } else {
+      console.error("Error inserting docs ", result);
+      res.status(500).send("Error inserting docs ", result);
+    }
+  } catch (error) {
+    console.error(error);
+    res.send(error);
   }
 });
 
@@ -69,13 +88,19 @@ router.post("/insert", async (req, res) => {
  * Returns 201 on success or 500 on failure.
  */
 router.post("/update", async (req, res) => {
-  let db = await ourMongo();
-  let result = await db.updateMany(req.body.filter, req.body.newValues);
-  if (Number(result.result.ok)) {
-    res.status(201).send(result.nModified + " docs updated successfully");
-  } else {
-    console.error("Error updating docs ", result);
-    res.status(500).send("Error updating docs ", result);
+  try {
+    await assertPassword(req);
+    let db = await ourMongo();
+    let result = await db.updateMany(req.body.filter, req.body.newValues);
+    if (Number(result.result.ok)) {
+      res.status(201).send(result.nModified + " docs updated successfully");
+    } else {
+      console.error("Error updating docs ", result);
+      res.status(500).send("Error updating docs ", result);
+    }
+  } catch (error) {
+    console.error(error);
+    res.send(error);
   }
 });
 
