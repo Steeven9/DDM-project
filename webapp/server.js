2d4a19e62c9bcbc4fda4c5a1b721319e0348bfdf
Joey <bevilacquajoey@gmail.com>
2021-11-30 11:13:39 +0100

    Simplify routes handling
    All of them but /mongo/insert are handled automatically by express
    
    Signed-off-by: Joey <bevilacquajoey@gmail.com>

--- webapp/server.js
+++ webapp/server.js
@@ -12,23 +12,7 @@ app.use(express.static(__dirname + "/public"));
 app.use("/api/neo4j", neo4j);
 app.use("/api/mongo", mongo);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-
-app.get("/password", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/password.html"));
-});
-
-app.get("/neo4j", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/neo4j.html"));
-});
-
-app.get("/mongo", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/mongo.html"));
-});
-
-app.get("/mongoInsert", (req, res) => {
+app.get("/mongo/insert", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/mongo/insert.html"));
 });
 
