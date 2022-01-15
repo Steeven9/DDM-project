92752853e4e49f120f6ea58fced14e3184573f03
Stefano Taillefert <stefano.taille@gmail.com>
2022-01-15 16:15:19 +0100

    Remove unused route

--- webapp/server.js
+++ webapp/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const neo4j = require("./routes/neo4j");
 const mongo = require("./routes/mongo");
-const elastic = require("./routes/elastic");
 
 const app = express();
 
@@ -11,7 +10,6 @@ app.use(express.static(__dirname + "/public"));
 
 app.use("/api/neo4j", neo4j);
 app.use("/api/mongo", mongo);
-app.use("/api/elastic", elastic);
 
 app.listen(process.env.PORT || 3000);
 console.info("DDM server running");
